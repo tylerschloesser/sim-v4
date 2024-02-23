bf961a0634d68e2ed998bd1d0c12ff1c5f009a22
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-23 09:54:08 -0800

    add app

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import invariant from 'tiny-invariant'
+import { App } from './app.js'
 import './index.scss'
 
 const container = document.getElementById('root')
 invariant(container)
 
-createRoot(container).render(<>TODO</>)
+createRoot(container).render(<App />)
