d8f08a6b9ee3dc2e3c73b19809fe11969384a05a
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-23 09:51:00 -0800

    react render

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,1 +1,7 @@
-console.log('hello world')
+import { createRoot } from 'react-dom/client'
+import invariant from 'tiny-invariant'
+
+const container = document.getElementById('root')
+invariant(container)
+
+createRoot(container).render(<>TODO</>)
