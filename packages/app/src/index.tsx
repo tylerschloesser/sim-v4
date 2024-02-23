4d3848269bdcd71439536b04f336900a2458be36
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-23 09:52:07 -0800

    add css

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import invariant from 'tiny-invariant'
+import './index.scss'
 
 const container = document.getElementById('root')
 invariant(container)
