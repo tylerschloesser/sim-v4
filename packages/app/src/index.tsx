9594449b1787e0fc64fe1feb38ec18650edd3b9b
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-26 15:31:42 -0800

    add done

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,10 +1,11 @@
-import { enablePatches } from 'immer'
+import { enableMapSet, enablePatches } from 'immer'
 import { createRoot } from 'react-dom/client'
 import invariant from 'tiny-invariant'
 import { App } from './app.js'
 import './index.scss'
 
 enablePatches()
+enableMapSet()
 
 const container = document.getElementById('root')
 invariant(container)
--- packages/app/src/render-patch.tsx
+++ packages/app/src/render-patch.tsx
@@ -1,5 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import { Updater } from 'use-immer'
+import React, {
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
+import { Updater, useImmer } from 'use-immer'
 import { getColor } from './color.js'
 import styles from './render-patch.module.scss'
 import { Patch, World } from './world.js'
@@ -20,6 +25,10 @@ export const RenderPatch = React.memo(function Circle({
 }: RenderPatchProps) {
   console.log(`render patch id=${id} count=${count}`)
 
+  const [pops, setPops] = useImmer<Set<string>>(new Set())
+
+  const nextPopId = useRef<number>(0)
+
   const [mine, setMine] = useState<boolean>(false)
 
   useEffect(() => {
@@ -30,22 +39,85 @@ export const RenderPatch = React.memo(function Circle({
     setWorld((draft) => {
       draft.pickaxe.patchId = id
     })
+
+    setPops((prev) => {
+      prev.add(`${nextPopId.current++}`)
+    })
   }, [mine])
+
+  const done = useCallback((id: string) => {
+    setPops((prev) => {
+      prev.delete(id)
+    })
+  }, [])
+
+  return (
+    <g data-group={`patch-${id}`}>
+      <circle
+        className={styles.patch}
+        onPointerDown={() => setMine(true)}
+        onPointerUp={() => setMine(false)}
+        onPointerLeave={() => setMine(false)}
+        cx={x}
+        cy={y}
+        r={radius}
+        style={
+          {
+            '--color': getColor(id),
+          } as React.CSSProperties
+        }
+      />
+      {[...pops].map((id) => (
+        <Pop key={id} id={id} x={x} y={y} done={done} />
+      ))}
+    </g>
+  )
+})
+
+interface PopProps {
+  id: string
+  x: number
+  y: number
+  done(id: string): void
+}
+
+const Pop = React.memo(function Pop({
+  id,
+  x,
+  y,
+  done,
+}: PopProps) {
+  const handle = useRef<number>()
+
+  useEffect(() => {
+    const start = self.performance.now()
+    const duration = 1000
+
+    function render() {
+      const elapsed = self.performance.now() - start
+      if (elapsed >= duration) {
+        done(id)
+        return
+      }
+
+      handle.current = self.requestAnimationFrame(render)
+    }
+    handle.current = self.requestAnimationFrame(render)
+
+    return () => {
+      if (handle.current) {
+        self.cancelAnimationFrame(handle.current)
+      }
+    }
+  }, [])
 
   return (
     <circle
-      className={styles.patch}
-      onPointerDown={() => setMine(true)}
-      onPointerUp={() => setMine(false)}
-      onPointerLeave={() => setMine(false)}
+      data-id={id}
+      className={styles.pop}
       cx={x}
       cy={y}
-      r={radius}
-      style={
-        {
-          '--color': getColor(id),
-        } as React.CSSProperties
-      }
+      r={0.25}
     />
   )
 })
