21df8fbd753a4b8c422b2caac8c2795b2e2ce971
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-28 14:14:43 -0800

    refactor button props

--- packages/app/src/render-primary-button.tsx
+++ packages/app/src/render-primary-button.tsx
@@ -10,56 +10,68 @@ export interface RenderPrimaryButtonProps {
   setWorld: Updater<World>
 }
 
+function getButtonProps(
+  cursor: Cursor,
+  setWorld: Updater<World>,
+) {
+  const disabled = cursor.patchId === null
+  const onPointerUp = disabled
+    ? undefined
+    : () => {
+        if (disabled) {
+          return
+        }
+        setWorld((draft) => {
+          invariant(draft.cursor.patchId)
+          const patch = draft.patches[draft.cursor.patchId]
+          invariant(patch)
+
+          const patchInventory =
+            draft.inventories[patch.inventoryId]
+          invariant(patchInventory)
+
+          const cursorInventory =
+            draft.inventories[draft.cursor.inventoryId]
+          invariant(cursorInventory)
+
+          const itemType = getPatchItemType(patchInventory)
+
+          const patchCount = patchInventory.items[itemType]
+          invariant(
+            typeof patchCount === 'number' &&
+              patchCount >= 1,
+          )
+          patchInventory.items[itemType] = patchCount - 1
+
+          if (patchCount === 1) {
+            delete draft.patches[patch.id]
+            delete draft.inventories[patch.inventoryId]
+            draft.cursor.patchId = null
+          }
+
+          const cursorCount =
+            cursorInventory.items[itemType]
+          cursorInventory.items[itemType] =
+            (cursorCount ?? 0) + 1
+        })
+      }
+  return { disabled, onPointerUp }
+}
+
 export const RenderPrimaryButton = React.memo(
   function RenderPrimaryButton({
     cursor,
     setWorld,
   }: RenderPrimaryButtonProps) {
-    const disabled = cursor.patchId === null
+    const { disabled, onPointerUp } = getButtonProps(
+      cursor,
+      setWorld,
+    )
     return (
       <button
         className={styles['primary-button']}
         disabled={disabled}
-        onPointerUp={() => {
-          if (disabled) {
-            return
-          }
-          setWorld((draft) => {
-            invariant(draft.cursor.patchId)
-            const patch =
-              draft.patches[draft.cursor.patchId]
-            invariant(patch)
-
-            const patchInventory =
-              draft.inventories[patch.inventoryId]
-            invariant(patchInventory)
-
-            const cursorInventory =
-              draft.inventories[draft.cursor.inventoryId]
-            invariant(cursorInventory)
-
-            const itemType =
-              getPatchItemType(patchInventory)
-
-            const patchCount =
-              patchInventory.items[itemType]
-            invariant(
-              typeof patchCount === 'number' &&
-                patchCount >= 1,
-            )
-            patchInventory.items[itemType] = patchCount - 1
-
-            if (patchCount === 1) {
-              delete draft.patches[patch.id]
-              delete draft.inventories[patch.inventoryId]
-            }
-
-            const cursorCount =
-              cursorInventory.items[itemType]
-            cursorInventory.items[itemType] =
-              (cursorCount ?? 0) + 1
-          })
-        }}
+        onPointerUp={onPointerUp}
       >
         Mine
       </button>
