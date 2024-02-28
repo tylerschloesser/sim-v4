c823aed47e57d5744858cba8a6dbb5a0052cc84b
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-27 20:11:54 -0800

    add primary button implementation

--- packages/app/src/render-primary-button.tsx
+++ packages/app/src/render-primary-button.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import invariant from 'tiny-invariant'
 import { Updater } from 'use-immer'
 import styles from './render-primary-button.module.scss'
-import { Cursor, World } from './world.js'
+import { Cursor, ItemType, World } from './world.js'
 
 export interface RenderPrimaryButtonProps {
   cursor: Cursor
@@ -13,11 +14,25 @@ export const RenderPrimaryButton = React.memo(
     cursor,
     setWorld,
   }: RenderPrimaryButtonProps) {
+    const disabled = cursor.patchId === null
     return (
       <button
         className={styles['primary-button']}
-        disabled={cursor.patchId === null}
-        onPointerUp={() => {}}
+        disabled={disabled}
+        onPointerUp={() => {
+          if (disabled) {
+            return
+          }
+          setWorld((draft) => {
+            const inventory =
+              draft.inventories[draft.cursor.inventoryId]
+            invariant(inventory)
+
+            const itemType = ItemType.enum.IronOre
+            const count = inventory.items[itemType]
+            inventory.items[itemType] = (count ?? 0) + 1
+          })
+        }}
       >
         Mine
       </button>
