4ed4aeafa9b99a9f4c84d8e3729d2c735032d490
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-28 17:32:57 -0800

    fix eslint

--- packages/app/src/tick-world.ts
+++ packages/app/src/tick-world.ts
@@ -1,6 +1,7 @@
 import { Updater } from 'use-immer'
 import { World } from './world.js'
 
-export function tickWorld(setWorld: Updater<World>): void {
+// eslint-disable-next-line
+export function tickWorld(_setWorld: Updater<World>): void {
   console.log('todo tick world')
 }
