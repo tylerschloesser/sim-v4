090f6d822d100226c02fb9ca0da467b4f133b3fd
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-28 17:53:44 -0800

    add tick smelter

---
+++ packages/app/src/tick-smelter.ts
@@ -0,0 +1,6 @@
+import { SmelterEntity, World } from './world.js'
+
+export function tickSmelter(
+  world: World,
+  entity: SmelterEntity,
+): void {}
--- packages/app/src/tick-world.ts
+++ packages/app/src/tick-world.ts
@@ -1,7 +1,18 @@
 import { Updater } from 'use-immer'
-import { World } from './world.js'
+import { tickSmelter } from './tick-smelter.js'
+import { EntityType, World } from './world.js'
 
 // eslint-disable-next-line
-export function tickWorld(_setWorld: Updater<World>): void {
-  console.log('todo tick world')
+export function tickWorld(setWorld: Updater<World>): void {
+  setWorld((world) => {
+    world.tick += 1
+
+    for (const entity of Object.values(world.entities)) {
+      switch (entity.type) {
+        case EntityType.enum.Smelter:
+          tickSmelter(world, entity)
+          break
+      }
+    }
+  })
 }
