af8a142e13a5b3fcc154c9a43411b2cd118a022d
Tyler Schloesser <tylerschloesser@gmail.com>
2024-03-06 14:50:05 -0800

    panel works

--- packages/app/src/render-panels.tsx
+++ packages/app/src/render-panels.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import invariant from 'tiny-invariant'
 import styles from './render-panels.module.scss'
+import { useCameraEffect } from './use-camera-effect.js'
+import { getScale } from './viewport.js'
 import { Entity, World, getEntity } from './world.js'
 
 interface RenderPanelsProps {
@@ -8,8 +11,24 @@ interface RenderPanelsProps {
 
 export const RenderPanels = React.memo(
   function RenderPanels({ world }: RenderPanelsProps) {
+    const ref = useRef<HTMLDivElement>(null)
+
+    useCameraEffect((camera, viewport) => {
+      const { x: vx, y: vy } = viewport.size
+      const scale = getScale(camera.zoom, vx, vy)
+
+      const { x: cx, y: cy } = camera.position
+
+      invariant(ref.current)
+      ref.current.style.setProperty('--scale', `${scale}`)
+      ref.current.style.setProperty('--cx', `${cx}`)
+      ref.current.style.setProperty('--cy', `${cy}`)
+
+      ref.current.style.setProperty('--vy', `${vy}`)
+    }, [])
+
     return (
-      <div className={styles.panels}>
+      <div className={styles.panels} ref={ref}>
         {Object.values(world.shapes).map(({ id }) => (
           <RenderPanel
             key={id}
@@ -28,5 +47,17 @@ interface RenderPanelProps {
 const RenderPanel = React.memo(function RenderPanel({
   entity,
 }: RenderPanelProps) {
-  return <>{entity.id}</>
+  return (
+    <div
+      className={styles.panel}
+      style={
+        {
+          '--x': `${entity.shape.position.x}`,
+          '--y': `${entity.shape.position.y}`,
+        } as React.CSSProperties
+      }
+    >
+      {entity.id}
+    </div>
+  )
 })
