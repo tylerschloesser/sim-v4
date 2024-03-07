ddc7b3c7e5db7aba9f9b0d8d39a367e9f54b9793
Tyler Schloesser <tylerschloesser@gmail.com>
2024-03-07 11:48:53 -0800

    refactor dist

--- packages/app/src/build.ts
+++ packages/app/src/build.ts
@@ -1,5 +1,6 @@
+import invariant from 'tiny-invariant'
 import { Vec2, vec2 } from './vec2.js'
-import { World } from './world.js'
+import { EntityShape, World } from './world.js'
 
 export function isBuildValid(
   position: Vec2,
@@ -7,9 +8,23 @@ export function isBuildValid(
   shapes: World['shapes'],
 ): boolean {
   for (const shape of Object.values(shapes)) {
-    const v = vec2.clone(shape.position)
-    vec2.sub(v, position)
-    if (vec2.len(v) < radius + shape.radius) {
+    const dist = vec2.dist(position, shape.position)
+    if (dist < radius + shape.radius) {
+      return false
+    }
+  }
+  return true
+}
+
+export function isEditValid(
+  position: Vec2,
+  shape: EntityShape,
+  shapes: World['shapes'],
+): boolean {
+  invariant(shapes[shape.id])
+  for (const peer of Object.values(shapes)) {
+    const dist = vec2.dist(peer.position, position)
+    if (dist < peer.radius + shape.radius) {
       return false
     }
   }
